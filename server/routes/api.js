f82ecda24ed2574c4f6ea85afb178f5ce6cc8e6e
awais7 <awaiszubari@gmail.com>
2021-06-14 18:29:36 +0500

    login api created

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,16 +15,36 @@ mongoose.connect(db, err => {
 router.get('/', (req, res) => {
     res.send("from api route")
 });
+
 router.post('/register', (req, res) => {
     let userData = req.body;
     let user = new User(userData);
-    user.save((error, registeredUser) =>{
-        if(error){
+    user.save((error, registeredUser) => {
+        if (error) {
             console.log(error);
-        }else{
+        } else {
             res.status(200).send(registeredUser);
         }
     });
 });
+
+router.post('/login', (req, res) => {
+    let userData = req.body;
+
+    User.findOne({ email: userData.email }, (error, user) => {
+        if (error) {
+            console.log(error);
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid email')
+            } else if (user.password !== userData.password) {
+                res.status(401).send('Invalid password')
+            } else {
+                res.status(200).send(user);
+            }
+        }
+    })
+});
+
 
 module.exports = router;
